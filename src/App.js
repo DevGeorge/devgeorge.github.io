baf17fb248ae9fb30a25f1d4166f46da67b62ba5
DevGeorge <georgehaddad1998@gmail.com>
2022-07-22 06:31:20 -0700

    add react-router-dom and initial layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import HomeLayout from "./pages/layouts/HomeLayout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomeLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Route>
+    </Routes>
   );
 }
-
+{
+  /* <div className="App">
+<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>Github Page still under construction, Please come back later</p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+  <img src="/images/IMG_8070.png" alt="" />
+  <img src="/images/IMG_8071.png" alt="" />
+  <img src="/images/IMG_8072.png" alt="" />
+  <img src="/images/IMG_8073.png" alt="" />
+</header>
+</div> */
+}
 export default App;
---
+++ src/pages/layouts/HomeLayout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router";
+
+const HomeLayout = () => {
+  return (
+    <div>
+      Still Work in progress
+      <Outlet />
+    </div>
+  );
+};
+
+export default HomeLayout;
